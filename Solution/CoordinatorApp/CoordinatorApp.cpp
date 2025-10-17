f7842677dc7f01405c34195571d98818b3d7c10d
/artem kasapov <kasapova79@gmail.com>
2025-10-17 13:33:03 +0300

    Finding Gemini Numbers
    Finding Gemini Numbers_C++

---
+++ Solution/CoordinatorApp/CoordinatorApp.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+#include <vector>
+#include <locale>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    long long totalStart = 10;
+    long long totalEnd = 100000000;
+    int numParts = 4;
+    long long range = (totalEnd - totalStart + 1) / numParts;
+    vector<PROCESS_INFORMATION> processes;
+
+    for (int i = 0; i < numParts; i++) {
+        long long start = totalStart + i * range;
+        long long end = (i == numParts - 1) ? totalEnd : start + range - 1;
+        string command = "WorkerApp.exe " + to_string(start) + " " + to_string(end);
+        cout << "Запуск: " << command << endl;
+        vector<char> buffer(command.begin(), command.end());
+        buffer.push_back('\0'); 
+
+        STARTUPINFOA si{};
+        PROCESS_INFORMATION pi{};
+        si.cb = sizeof(si);
+
+        if (CreateProcessA(
+            NULL,
+            buffer.data(),
+            NULL, NULL, FALSE,
+            0, NULL, NULL,
+            &si, &pi))
+        {
+            processes.push_back(pi);
+        }
+        else {
+            cout << "Ошибка запуска WorkerApp!" << endl;
+        }
+    }
+
+    for (auto& pi : processes) {
+        WaitForSingleObject(pi.hProcess, INFINITE);
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+    }
+
+    cout << "Все процессы завершены." << endl;
+    return 0;
+}
---
+++ Solution/WorkerApp/WorkerApp.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <locale>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <thread>
+
+using namespace std;
+
+bool isPrime(long long n) {
+    if (n < 2) return false;
+    if (n == 2 || n == 3) return true;
+    if (n % 2 == 0) return false;
+    for (long long i = 3; i * i <= n; i += 2)
+        if (n % i == 0) return false;
+    return true;
+}
+
+int main(int argc, char* argv[]) {
+    setlocale(LC_ALL, "Russian");
+    if (argc < 3) {
+        cout << "Использование: WorkerApp <start> <end>\n";
+        return 1;
+    }
+
+    long long start = atoll(argv[1]);
+    long long end = atoll(argv[2]);
+
+    cout << "Процесс обрабатывает диапазон [" << start << ", " << end << "]\n";
+    cout << "Загрузка...";
+    long long countTwins = 0;
+    long long prevPrime = -1;
+    for (long long i = start; i <= end; i++) {
+        if (isPrime(i)) {
+            if (prevPrime != -1 && i - prevPrime == 2)
+                countTwins++;
+            prevPrime = i;
+        }
+    }
+
+    cout << "\nНайдено " << countTwins << " пар чисел-близнецов в диапазоне ["
+        << start << " - " << end << "]\n";
+
+    system("pause");
+    return 0;
+}
